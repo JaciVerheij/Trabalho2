783e1899558b905f11bbd97154d68d0811ca11e8
LucasTalen <103639813+LucasTalen@users.noreply.github.com>
2023-04-04 21:44:58 -0300

    add modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Dupla: Jaci Verheij e Lucas de Paula
+
 function validarLogin(){
     const inputEmail = document.getElementById("campo_email")
     const inputSenha = document.getElementById("campo_senha")
@@ -10,15 +12,37 @@ function validarLogin(){
         botao_entrar = window.open("~/../index.html")
     }
 }
+
 function sair(){
     btn_sair = window.open("~/../login.html")
+    localStorage.removeItem('token')
 }
 
 function mostrarNome(nomeUsuario){
-    document.write("Olá ", nomeUsuario, ", seja bem-vinda!")
+    document.write("Olá ", nomeUsuario, ", seja bem-vindo!")
 }
 
 const $html = document.querySelector('html')
 const $checkbox =  document.addEventListener('change',function(){
     $html.classList.toggle('dark_mode')
+})
+
+var count = 0
+
+$(function(){
+    let task_list = $("#task_list")
+    $("myForm").submit(function(event){
+        let valuesInForm = $(this).serializeArray()
+        console.table(valuesInForm)
+        if(count <= 6){
+            $("#taskTable").append('<tr> <td>' + valuesInForm[0].value + '</td>' + 
+            '<td>' + valuesInForm[1].value + '</td>' +
+            '<td>' + valuesInForm[2].value + '</td> </tr>')
+
+        }else{
+            alert("O limite foi atingido")
+        }
+        count += 1
+        event.preventDefault();
+    })
 })
